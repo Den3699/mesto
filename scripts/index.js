dd13979de6f3be7d11d0df7296f86a826b3aa851
Denis Budkov <Chukcha2017@bk.ru>
2023-01-08 00:36:53 +0400

    Final

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const formElement = popupElement.querySelector('popuo__content');
+const nameInput = popupElement.querySelector('.popup__name');
+const jobInput = popupElement.querySelector('popup__occupation');
+const profileName = document.querySelector('profile__name');
+const profileOccupation = document.querySelector('.profile__occupation');
+
+
+const openPopup = function() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileOccupation.textContent;
+};
+
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_opened');
+};
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileOccupation.textContent = jobInput.value;
+  closePopup();
+}
+
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit' , formSubmitHandler);
